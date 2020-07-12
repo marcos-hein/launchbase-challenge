dd99314c3e9bb2c2335a8c482943ae2c6a318075
Vinicius Hein <vinicius.hein@gmail.com>
2020-07-12 18:49:59 -0300

    organized folders

---
+++ challenge3-1/server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const nunjucks = require("nunjucks")
+
+const server = express()
+
+server.use(express.static("public"))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function(req, res){
+    return res.render("about-rocketseat")
+})
+
+server.get("/content", function(req, res){
+    return res.render("content")
+})
+
+server.listen(5000, function(){
+    console.log("Server is a LIVEEE")
+})
