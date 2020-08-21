054aa9de48e231821e62cce74df3001065c35921
Vinicius Hein <vinicius.hein@gmail.com>
2020-08-20 22:12:20 -0300

    create-teacher page created

--- challenge4-3/routes.js
+++ challenge4-3/routes.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const routes = express.Router()
+// const teachers = require('./teachers')
 
-routes.get('/', function(req, res){
-    return res.render('teachers')
+routes.get('/', function(req, res) {
+    return res.redirect('/teachers')
+})
+
+routes.get('/teachers', function(req, res){
+    return res.render('teachers/teachers')
+})
+
+routes.get('create-teacher', function(req, res) {
+    return res.render('teachers/create-teacher')
 })
 
 module.exports = routes
