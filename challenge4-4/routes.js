1392499577b78f3cc73c3d45da84be79769c7a43
Vinicius Hein <vinicius.hein@gmail.com>
2020-08-26 22:42:57 -0300

    teacher data formatting

---
+++ challenge4-4/routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const routes = express.Router()
+const teachers = require('./teachers')
+
+routes.get('/', function(req, res) {
+    return res.redirect('/teachers')
+})
+
+routes.get('/teachers', function(req, res){
+    return res.render('teachers/teachers')
+})
+
+routes.get('/teachers/create', function(req, res) {
+    return res.render('teachers/create-teacher')
+})
+
+routes.get('/teachers/:id', teachers.show)
+
+
+
+// edit
+// routes.get('teachers/id/edit', teachers.edit)
+
+
+routes.post('/teachers', teachers.post)
+
+module.exports = routes
