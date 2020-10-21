84f8a8cc4b7d115721f9f68104fa5c0192c945eb
Vinicius Hein <vinicius.hein@gmail.com>
2020-10-20 22:00:05 -0300

    refactoring project

---
+++ challenge5-1/src/app/controllers/teachers.js
@@ -0,0 +1,148 @@
+const fs = require('fs')
+const data = require('../data.json')
+const { age, graduation, date } = require('../utils')
+
+exports.index = function(req, res){
+    const teachers = data.teachers.map(function(teacher) {
+        return {
+            ...teacher,
+            services: teacher.services.split(","),
+
+        }
+    })
+
+    return res.render('teachers/teachers', { teachers })
+}
+
+// create
+exports.create = function(req, res) {
+    return res.render('teachers/create-teacher')
+}
+
+// post
+exports.post = function(req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == ""){
+            return res.send('Please, fill all fields!')
+        }
+    }
+
+    let { avatar_url, name, birth, schooling, type_of_class, services } = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.teachers.length + 1)
+
+
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        schooling,
+        type_of_class,
+        services,
+        created_at
+    })
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+        if (err) {
+            return res.send('Write file error!')
+        }
+
+        return res.redirect('/teachers')
+    })
+    
+    // return res.send(req.body)
+}
+
+// show
+exports.show = function(req, res) {
+    const { id } = req.params
+
+    const foundTeacher = data.teachers.find(function(teacher) {
+        return teacher.id == id
+    })
+
+    if (!foundTeacher) return res.send('Teacher not found!')
+
+    const teacher = {
+        ...foundTeacher,
+        age: age(foundTeacher.birth),
+        schooling: graduation(foundTeacher.schooling),
+        services: foundTeacher.services.split(","),
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundTeacher.created_at)
+    }
+
+    return res.render('./teachers/show', { teacher })
+}
+
+// edit
+exports.edit = function(req, res) {
+    const { id } = req.params
+
+    const foundTeacher = data.teachers.find(function(teacher) {
+        return teacher.id == id
+    })
+
+    if (!foundTeacher) return res.send('Teacher not found')
+
+    const teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth)
+    }
+
+
+    return res.render('./teachers/edit', { teacher })
+}
+
+// put
+exports.update = function(req, res) {
+    const { id } = req.body
+
+    let index = 0
+    
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex) {
+        if (id == teacher.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundTeacher) return res.send('Teacher not found!')
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+        id: Number(id)
+    }
+
+    data.teachers[index] = teacher
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send('Teacher not found!')
+
+        return res.redirect(`/teachers/${id}`)
+    })
+}
+
+// delete
+exports.delete = function(req, res) {
+    const { id } = req.body
+
+    const filteredTeachers = data.teachers.filter(function(teacher) {
+        return teacher.id != id
+    })
+
+    data.teachers = filteredTeachers
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send('Write file error!')
+
+        return res.redirect('/teachers')
+    })
+}
---
+++ challenge5-1/src/lib/utils.js
@@ -0,0 +1,65 @@
+const { timeStamp } = require("console")
+
+module.exports = {
+    age: function(timestamp) {
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        const month = today.getMonth() - birthDate.getMonth()
+
+        if (month < 0 ||
+            month == 0 &&
+            today.getDate() <= birthDate.getDate() ){
+            age -= 1
+        }
+
+        return age
+
+    },
+    graduation: function(schoolingNumber) {
+        const schooling = [
+            "Ensino Médio Completo",
+            "Ensino Superior Completo",
+            "Mestrado",
+            "Doutorado"
+        ]
+
+        const position = schoolingNumber - 1
+
+        return schooling[position]
+    },
+    grade: function(schoolYear) {
+        const schoolYears = {
+            1: "5º Ano do Ensino Fundamental",
+            2: "6º Ano do Ensino Fundamental",
+            3: "7º Ano do Ensino Fundamental",
+            4: "8º Ano do Ensino Fundamental",
+            5: "9º Ano do Ensino Fundamental",
+            6: "1º Ano do Ensino Médio",
+            7: "2º Ano do Ensino Médio",
+            8: "3º Ano do Ensino Médio",
+        }
+
+        for (const option in schoolYears) {
+            if (option == schoolYear) {
+                return schoolYears[option];   
+            }
+        }  
+    },
+    date: function(timestamp){
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear()
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        const day = `0${date.getUTCDate() + 1}`.slice(-2)
+
+        
+        return {
+            iso:`${year}-${month}-${day}`,
+            birthDay: `${day}/${month}`
+        }
+    }
+}
+
+// teste
