1b1e98df30a4bfae3fe4feafcac59df14e3488d2
Vinicius Hein <vinicius.hein@gmail.com>
2020-11-05 21:49:06 -0300

    create method created

--- challenge5-2/src/app/controllers/teachers.js
+++ challenge5-2/src/app/controllers/teachers.js
@@ -20,34 +20,11 @@ module.exports = {
                 return res.send('Please, fill all fields!')
             }
         }
-
-        let { avatar_url, name, birth, schooling, type_of_class, services } = req.body
-
-        birth = Date.parse(birth)
-        const created_at = Date.now()
-        const id = Number(data.teachers.length + 1)
-
-
-        data.teachers.push({
-            id,
-            avatar_url,
-            name,
-            birth,
-            schooling,
-            type_of_class,
-            services,
-            created_at
-        })
-
-        fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
-            if (err) {
-                return res.send('Write file error!')
-            }
-
-            return res.redirect('/teachers')
-        })
         
-        // return res.send(req.body)
+         Teacher.create(req.body, function(teacher) {
+             return res.redirect(`/teachers/${teacher.id}`)
+         })
+        
     },
     show(req, res) {
         const { id } = req.params
--- challenge5-2/src/app/models/Teacher.js
+++ challenge5-2/src/app/models/Teacher.js
@@ -1,12 +1,46 @@
-// const { age, graduation, date } = require('../../lib/utils')
+const { age, graduation, date } = require('../../lib/utils')
 const db = require("../../config/db")
 
 module.exports = {
     all(callback) {
-        db.query(`SELECT * FROM teachers`, function(err, results) {
+        db.query(`
+        SELECT *
+        FROM teachers
+        ORDER BY teachers.name ASC`, function(err, results) {
             if(err) throw `Database error! ${err}`
 
             callback(results.rows)
         })
+    },
+    create(data, callback) {
+        const query = `
+            INSERT INTO teachers (
+                name,
+                avatar_url,
+                birth_date,
+                education_level,
+                class_type,
+                subjects_taught,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING id
+        `
+
+        const values = [
+            data.name,
+            data.avatar_url,
+            date(data.birth_date).iso,
+            graduation(data.education_level),
+            data.class_type,
+            data.subjects_taught,
+            date(Date.now()).iso
+        ]
+
+        db.query(query, values, function(err, results) {
+            if (err) throw `Database error! ${err}`
+
+            callback(results.rows[0])
+        })
+
     }
 }
